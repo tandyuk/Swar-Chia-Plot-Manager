820a80b9724ab14d864eaa42c459121146ea15e2
James Tandy <james@jamestandy.com>
2021-05-26 00:11:33 +0100

    Added a couple of missing references to post_plot_script

--- plotmanager/library/utilities/commands.py
+++ plotmanager/library/utilities/commands.py
@@ -131,7 +131,7 @@ def json_output():
 
 def view(loop=True):
     chia_location, log_directory, config_jobs, manager_check_interval, max_concurrent, max_for_phase_1, \
-        minimum_minutes_between_jobs, progress_settings, notification_settings, debug_level, view_settings, \
+        minimum_minutes_between_jobs, post_plot_script, progress_settings, notification_settings, debug_level, view_settings, \
         instrumentation_settings = get_config_info()
     view_check_interval = view_settings['check_interval']
     system_drives = get_system_drives()
@@ -191,6 +191,6 @@ def view(loop=True):
 
 def analyze_logs():
     chia_location, log_directory, config_jobs, manager_check_interval, max_concurrent, max_for_phase_1, \
-        minimum_minutes_between_jobs, progress_settings, notification_settings, debug_level, view_settings, \
+        minimum_minutes_between_jobs, post_plot_script, progress_settings, notification_settings, debug_level, view_settings, \
         instrumentation_settings = get_config_info()
     analyze_log_times(log_directory)
